zl1-aicp/packages_services_Telephony

[4/4] Telephony: Smart Phone Call
Put on your ear, and this will automatically call
current number in message or dialer

AICP:
modified for AICP source
PS2: fix build

Change-Id: I2cf8f44094c6017b7ab238490920aa0809d9ce4c

src/com/android/phone/CallFeaturesSetting.java
@@ -204,6 +204,7 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_NON_INTRUSIVE_INCALL_KEY = "button_non_intrusive_incall";
     private static final String BUTTON_CALL_END_SOUND_KEY = "button_call_end_sound";
+    private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
     private static final String FLIP_ACTION_KEY = "flip_action";
 
     private static final String SWITCH_ENABLE_FORWARD_LOOKUP =
@@ -312,6 +313,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private PreferenceScreen mButtonBlacklist;
     private ListPreference mFlipAction;
     private CheckBoxPreference mCallEndSound;
+    private CheckBoxPreference mSmartCall;
     private SwitchPreference mEnableForwardLookup;
     private SwitchPreference mEnablePeopleLookup;
     private SwitchPreference mEnableReverseLookup;
@@ -582,6 +584,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mNonIntrusiveInCall){
             Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.NON_INTRUSIVE_INCALL,
                     mNonIntrusiveInCall.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mSmartCall){
+            Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
             return true;
         } else if (preference == mCallEndSound){
             Settings.System.putInt(getContentResolver(), Settings.System.CALL_END_SOUND,
@@ -1728,6 +1734,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mCallEndSound.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.CALL_END_SOUND, 1) == 0 ? false : true);
 
+        mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
+        mSmartCall.setChecked(Settings.AOKP.getInt(getContentResolver(),
+                Settings.AOKP.SMART_PHONE_CALLER, 0) != 0 ? true : false);
+
         mEnableForwardLookup = (SwitchPreference)
                 findPreference(SWITCH_ENABLE_FORWARD_LOOKUP);
         mEnablePeopleLookup = (SwitchPreference)
@@ -1796,7 +1806,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             }
         };
 
-        // Blacklist screen - Needed for setting summary 
+        // Blacklist screen - Needed for setting summary
         mButtonBlacklist = (PreferenceScreen) prefSet.findPreference(BUTTON_BLACKLIST);
 
         ActionBar actionBar = getActionBar();

wanginator/android_packages_services_telephony

Conflicts:
	res/values/strings_custom.xml
	res/xml/call_feature_setting.xml
	src/com/android/phone/CallFeaturesSetting.java

src/com/android/phone/CallFeaturesSetting.java
@@ -200,7 +200,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             "sip_call_options_wifi_only_key";
     private static final String SIP_SETTINGS_CATEGORY_KEY =
             "sip_settings_category_key";
-
+    private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
     private static final String SWITCH_ENABLE_FORWARD_LOOKUP =
             "switch_enable_forward_lookup";
     private static final String SWITCH_ENABLE_REVERSE_LOOKUP =
@@ -296,7 +296,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private Preference mVoicemailNotificationRingtone;
     private CheckBoxPreference mVoicemailNotificationVibrate;
     private SipSharedPreferences mSipSharedPreferences;
-
+    private CheckBoxPreference mSmartCall;
     private SwitchPreference mEnableForwardLookup;
     private SwitchPreference mEnableReverseLookup;
     private ListPreference mChooseForwardLookupProvider;
@@ -505,9 +505,14 @@ public class CallFeaturesSetting extends PreferenceActivity
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSubMenuVoicemailSettings) {
             return true;
+	} else if (preference == mSmartCall){
+            Settings.System.putInt(getContentResolver(), Settings.System.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
+            return true;
         } else if (preference == mPlayDtmfTone) {
             Settings.System.putInt(getContentResolver(), Settings.System.DTMF_TONE_WHEN_DIALING,
                     mPlayDtmfTone.isChecked() ? 1 : 0);
+
         } else if (preference == mButtonDTMF) {
             return true;
         } else if (preference == mButtonTTY) {
@@ -1668,6 +1673,9 @@ public class CallFeaturesSetting extends PreferenceActivity
                 throw new IllegalStateException("Unexpected phone type: " + phoneType);
             }
         }
+        mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
+        mSmartCall.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.SMART_PHONE_CALLER, 0) != 0 ? true : false);
 
         mEnableForwardLookup = (SwitchPreference)
                 findPreference(SWITCH_ENABLE_FORWARD_LOOKUP);
@@ -1732,6 +1740,12 @@ public class CallFeaturesSetting extends PreferenceActivity
             }
         };
 
+<<<<<<< HEAD
+=======
+        // Blacklist screen - Needed for setting summary
+        mButtonBlacklist = (PreferenceScreen) prefSet.findPreference(BUTTON_BLACKLIST);
+
+>>>>>>> b77926c... [4/4] Telephony: Smart Phone Call
         ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             // android.R.id.home will be triggered in onOptionsItemSelected()